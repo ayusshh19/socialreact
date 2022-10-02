b411faa8dbb16889661b0d34cf8fc34b2749a245
ayussh19 <cm.b.49ayush.shukla@gmail.com>
2022-10-02 12:23:25 +0530

    profile

--- socialreact/src/profile/profile.js
+++ socialreact/src/profile/profile.js
@@ -23,14 +23,27 @@ function Profile() {
     React.useEffect(() => {
         getUsers();
     }, [])
-
-    console.log(profileedit)
+console.log(userdatas)
+    // console.log(profileedit)
     return (
         <div className="container">
             {userdatas.map((item)=>{
                 return(
                     item.id==profid.profid?<>
-                    <img alt="ayush" src={item.image}></img></>:''
+                    <img alt="ayush" src={item.image}></img>
+                    <div className="maincontent">
+                    <h2>{item.firstName} {item.lastName}</h2>
+                    <h4>Birthdate : {item.birthDate}</h4>
+                    <h4>Address : {item.address.address}</h4>
+                    <h4>Age : {item.age}</h4>
+                    <h4>Email : {item.email}</h4>
+                    <h4>Gender : {item.gender}</h4>
+                    <h4>Maidenanme : {item.maidenName}</h4>
+                    <h4>University : {item.university}</h4>
+                    <h4>Username : {item.username}</h4>
+                    </div>
+                    </>:''
+
                 )
             })}
             
